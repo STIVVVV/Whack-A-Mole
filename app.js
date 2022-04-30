fc95f45b018e2d7125117615850798212f0fd7ff
STIVVVV <stevencanfield2000@gmail.com>
2022-04-30 00:00:02 -0400

    Finished Whack a mole

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
 
 let result = 0
+let hitPostion
+let currentTime = 60
+let timerId = null
+let countDownTimerId = setInterval(countDown, 1000)
+
+moveMole()
 
 function randomSquare()
 {
@@ -14,6 +20,38 @@ function randomSquare()
 
     let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('mole')
+
+    hitPostion = randomSquare.id
 }
 
-randomSquare()
+squares.forEach(square => 
+{
+    square.addEventListener('mousedown', () => 
+    {
+        if(square.id == hitPostion)
+        {
+            result++;
+            console.log(result)
+            score.textContent = result
+            hitPostion = null
+        }
+    }) 
+})
+
+function moveMole()
+{
+    timerId = setInterval(randomSquare, 500)
+}
+
+function countDown()
+{
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0)
+    {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert('GAME OVER! Your final score is ' + result)
+    }
+}
